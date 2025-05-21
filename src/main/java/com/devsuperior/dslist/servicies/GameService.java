7280f01a0166f4cda43cd565b723e389d68eb5ec
Marcelo <cordolla10@gmail.com>
2025-05-21 18:59:56 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/servicies/GameService.java
+++ src/main/java/com/devsuperior/dslist/servicies/GameService.java
@@ -5,6 +5,7 @@ import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -13,7 +14,7 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
-    public List<GameMinDTO> findAll(){
+    public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
